16daf2bba441be72f268ff178c909f2c794b2493
Martin Mroč <37422373+MartinMroc95@users.noreply.github.com>
2022-10-03 15:07:11 +0200

    Add `react-native-paper`

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,42 @@
+import * as React from 'react'
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
-import { Component } from './components/Component'
+import { StyleSheet, Text, View, Button } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Provider as PaperProvider, MD3LightTheme as DefaultTheme } from 'react-native-paper'
+
+import HomeScreen from './screens/Home'
+import DetailsScreen from './screens/Details'
+
+const Tab = createBottomTabNavigator()
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'tomato',
+    secondary: 'yellow',
+  },
+}
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working o</Text>
-      <StatusBar style="auto" />
-      <Component />
-    </View>
+    <PaperProvider theme={theme}>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Details" component={DetailsScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
