04df28f5f14a688ece2b28c53dce482f6515b9be
Martin Mroč <37422373+MartinMroc95@users.noreply.github.com>
2022-10-05 16:56:42 +0200

    Modify `firebase.ts`

--- firebase.ts
+++ firebase.ts
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import Constants from 'expo-constants'
 import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -16,7 +18,9 @@ const firebaseConfig = {
   appId: Constants.manifest?.extra?.firebaseAppId,
 }
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig)
 
-export default app
+const db = getFirestore(app)
+const auth = getAuth(app)
+
+export { auth, db }
