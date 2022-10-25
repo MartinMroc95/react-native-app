8b01db9675a4daeb5771d22be73bcb4872695adb
Martin Mroč <37422373+MartinMroc95@users.noreply.github.com>
2022-10-25 16:06:04 +0200

    Rework SignUpScreen

--- src/screens/SignUp/SignUpScreen.tsx
+++ src/screens/SignUp/SignUpScreen.tsx
@@ -1,90 +1,150 @@
 import React from 'react'
 import { Controller, SubmitHandler, useForm } from 'react-hook-form'
-import { Text, View } from 'react-native'
-import { Button, TextInput } from 'react-native-paper'
 import { FirebaseError } from '@firebase/util'
 import { yupResolver } from '@hookform/resolvers/yup'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
 import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth'
-import { signUpValidationScheme, styles } from './constants'
+import {
+  Box,
+  Button,
+  Center,
+  FormControl,
+  Heading,
+  Input,
+  VStack,
+  WarningOutlineIcon,
+} from 'native-base'
+import { Routes, UnAuthStackParamList } from 'navigation/constants'
+import { defaultSignUpFormValues, signUpValidationScheme } from './constants'
 
 type SignUpFormData = {
   email: string
   password: string
+  confirmPassword: string
 }
 
-const auth = getAuth()
+type SignUpProps = NativeStackScreenProps<UnAuthStackParamList, Routes.SignUp>
 
-const SignUpScreen: React.FC<NativeStackScreenProps<any>> = ({ navigation }) => {
+const SignUpScreen: React.FC<SignUpProps> = ({ navigation }) => {
+  const auth = getAuth()
+
   const {
     control,
     handleSubmit,
     formState: { errors },
   } = useForm<SignUpFormData>({
-    defaultValues: {
-      email: '',
-      password: '',
-    },
+    defaultValues: defaultSignUpFormValues,
     resolver: yupResolver(signUpValidationScheme),
   })
 
   const onSubmit: SubmitHandler<SignUpFormData> = async ({ email, password }) => {
     try {
       await createUserWithEmailAndPassword(auth, email, password)
-      navigation.navigate('Sign In')
+      navigation.navigate(Routes.SignIn)
     } catch (error) {
       if (error instanceof FirebaseError) {
         console.log('error', error.message)
-        throw new Error(error.message)
+        // throw new Error(error.message)
       }
     }
   }
 
   return (
-    <View style={styles.container}>
-      <View>
-        <Controller
-          control={control}
-          render={({ field: { onChange, onBlur, value } }) => (
-            <TextInput
-              placeholder="Email"
-              mode="outlined"
-              style={styles.control}
-              onBlur={onBlur}
-              onChangeText={onChange}
-              value={value}
-            />
-          )}
-          name="email"
-        />
-        {errors.email && <Text style={styles.error}>{errors.email.message}</Text>}
-        <Controller
-          control={control}
-          render={({ field: { onChange, onBlur, value } }) => (
-            <TextInput
-              placeholder="Password"
-              mode="outlined"
-              style={styles.control}
-              onBlur={onBlur}
-              onChangeText={onChange}
-              value={value}
-              secureTextEntry={true}
-            />
-          )}
-          name="password"
-        />
-        {errors.password && <Text style={styles.error}>{errors.password.message}</Text>}
-        <Button
-          mode="elevated"
-          style={styles.control}
-          onPress={() => {
-            void handleSubmit(onSubmit)()
+    <Center bgColor="white" flex={1} w="100%">
+      <Box safeArea p="2" w="90%" maxW="290" py="8">
+        <Heading
+          size="lg"
+          color="coolGray.800"
+          _dark={{
+            color: 'warmGray.50',
           }}
+          fontWeight="semibold"
         >
-          Sign up
-        </Button>
-      </View>
-    </View>
+          Welcome
+        </Heading>
+        <Heading
+          mt="1"
+          color="coolGray.600"
+          _dark={{
+            color: 'warmGray.200',
+          }}
+          fontWeight="medium"
+          size="xs"
+        >
+          Sign up to continue!
+        </Heading>
+        <VStack space={2} mt="5">
+          <FormControl isInvalid={!!errors.email}>
+            <FormControl.Label>Email</FormControl.Label>
+            <Controller
+              control={control}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <Input
+                  type="text"
+                  placeholder="Email"
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                />
+              )}
+              name="email"
+            />
+            <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
+              {errors.email && errors.email.message}
+            </FormControl.ErrorMessage>
+          </FormControl>
+          <FormControl isInvalid={!!errors.password}>
+            <FormControl.Label>Password</FormControl.Label>
+            <Controller
+              control={control}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <Input
+                  type="password"
+                  placeholder="Password"
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                  secureTextEntry={true}
+                />
+              )}
+              name="password"
+            />
+            <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
+              {errors.password && errors.password.message}
+            </FormControl.ErrorMessage>
+          </FormControl>
+          <FormControl isInvalid={!!errors.confirmPassword}>
+            <FormControl.Label>Confirm Password</FormControl.Label>
+            <Controller
+              control={control}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <Input
+                  type="password"
+                  placeholder="Confirm Password"
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                  secureTextEntry={true}
+                />
+              )}
+              name="confirmPassword"
+            />
+            <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
+              {errors.confirmPassword && errors.confirmPassword.message}
+            </FormControl.ErrorMessage>
+          </FormControl>
+          <Button
+            onPress={() => {
+              void handleSubmit(onSubmit)()
+            }}
+            mt="4"
+            colorScheme="indigo"
+          >
+            Sign up
+          </Button>
+        </VStack>
+      </Box>
+    </Center>
   )
 }
 
--- src/screens/SignUp/constants.ts
+++ src/screens/SignUp/constants.ts
@@ -1,23 +1,4 @@
-import { StyleSheet } from 'react-native'
 import * as yup from 'yup'
-
-export const styles = StyleSheet.create({
-  container: {
-    width: '100%',
-    height: '100%',
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  control: {
-    width: 200,
-    maxHeight: 60,
-    marginTop: 10,
-  },
-  error: {
-    color: 'red',
-  },
-})
 
 export const signUpValidationScheme = yup
   .object({
@@ -26,5 +7,15 @@ export const signUpValidationScheme = yup
       .email('Email must be a valid email.')
       .required('Email is a required field.'),
     password: yup.string().required('Password is a required field.'),
+    confirmPassword: yup
+      .string()
+      .oneOf([yup.ref('password'), null], "Passwords don't match.")
+      .required('Confirm Password is a required field.'),
   })
   .required()
+
+export const defaultSignUpFormValues = {
+  email: '',
+  password: '',
+  confirmPassword: '',
+}
