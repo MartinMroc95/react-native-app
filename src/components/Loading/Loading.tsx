b9866e93a69f50375d7b62071aa74951a19e87be
Martin Mroč <37422373+MartinMroc95@users.noreply.github.com>
2022-10-25 20:25:10 +0200

    Modify `Loading` component

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -2,7 +2,7 @@ import { View } from 'native-base'
 import React from 'react'
 import { ActivityIndicator } from 'react-native'
 
-export const Loading = () => {
+export const Loading: React.FC = () => {
   return (
     <View flex={1} justifyContent="center">
       <ActivityIndicator color={'#000'} animating={true} size="small" />
