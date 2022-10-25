f2d115e7f5dae1ed7556b7ee4c24a1633a091bfb
Martin Mroč <37422373+MartinMroc95@users.noreply.github.com>
2022-10-25 20:40:12 +0200

    Modify `SignUpScreen`

--- src/screens/SignUp/SignUpScreen.tsx
+++ src/screens/SignUp/SignUpScreen.tsx
@@ -16,11 +16,13 @@ import {
 } from 'native-base'
 import { Routes, UnAuthStackParamList } from 'navigation/constants'
 import { defaultSignUpFormValues, signUpValidationScheme } from './constants'
+import FormTextInput from 'components/FormTextInput'
+import FormPasswordInput from 'components/FormPasswordInput'
 
 type SignUpFormData = {
-  email: string
-  password: string
-  confirmPassword: string
+  email?: string
+  password?: string
+  confirmPassword?: string
 }
 
 type SignUpProps = NativeStackScreenProps<UnAuthStackParamList, Routes.SignUp>
@@ -39,8 +41,10 @@ const SignUpScreen: React.FC<SignUpProps> = ({ navigation }) => {
 
   const onSubmit: SubmitHandler<SignUpFormData> = async ({ email, password }) => {
     try {
-      await createUserWithEmailAndPassword(auth, email, password)
-      navigation.navigate(Routes.SignIn)
+      if (email && password) {
+        await createUserWithEmailAndPassword(auth, email, password)
+        navigation.navigate(Routes.SignIn)
+      }
     } catch (error) {
       if (error instanceof FirebaseError) {
         console.log('error', error.message)
@@ -74,65 +78,27 @@ const SignUpScreen: React.FC<SignUpProps> = ({ navigation }) => {
           Sign up to continue!
         </Heading>
         <VStack space={2} mt="5">
-          <FormControl isInvalid={!!errors.email}>
-            <FormControl.Label>Email</FormControl.Label>
-            <Controller
-              control={control}
-              render={({ field: { onChange, onBlur, value } }) => (
-                <Input
-                  type="text"
-                  placeholder="Email"
-                  onBlur={onBlur}
-                  onChangeText={onChange}
-                  value={value}
-                />
-              )}
-              name="email"
-            />
-            <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
-              {errors.email && errors.email.message}
-            </FormControl.ErrorMessage>
-          </FormControl>
-          <FormControl isInvalid={!!errors.password}>
-            <FormControl.Label>Password</FormControl.Label>
-            <Controller
-              control={control}
-              render={({ field: { onChange, onBlur, value } }) => (
-                <Input
-                  type="password"
-                  placeholder="Password"
-                  onBlur={onBlur}
-                  onChangeText={onChange}
-                  value={value}
-                  secureTextEntry={true}
-                />
-              )}
-              name="password"
-            />
-            <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
-              {errors.password && errors.password.message}
-            </FormControl.ErrorMessage>
-          </FormControl>
-          <FormControl isInvalid={!!errors.confirmPassword}>
-            <FormControl.Label>Confirm Password</FormControl.Label>
-            <Controller
-              control={control}
-              render={({ field: { onChange, onBlur, value } }) => (
-                <Input
-                  type="password"
-                  placeholder="Confirm Password"
-                  onBlur={onBlur}
-                  onChangeText={onChange}
-                  value={value}
-                  secureTextEntry={true}
-                />
-              )}
-              name="confirmPassword"
-            />
-            <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
-              {errors.confirmPassword && errors.confirmPassword.message}
-            </FormControl.ErrorMessage>
-          </FormControl>
+          <FormTextInput
+            control={control}
+            name="email"
+            label="Email"
+            placeholder="Enter email"
+            errorMessage={errors.email && errors.email.message}
+          />
+          <FormPasswordInput
+            control={control}
+            name="password"
+            label="Password"
+            placeholder="Enter password"
+            errorMessage={errors.password && errors.password.message}
+          />
+          <FormPasswordInput
+            control={control}
+            name="confirmPassword"
+            label="Confirm Password"
+            placeholder="Enter confirm password"
+            errorMessage={errors.confirmPassword && errors.confirmPassword.message}
+          />
           <Button
             onPress={() => {
               void handleSubmit(onSubmit)()
