fccad5095a2cdff19aad25f61802e62f8d86ba21
Martin Mroč <37422373+MartinMroc95@users.noreply.github.com>
2022-12-01 16:17:20 +0100

    Update graphql to v4

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,39 +1,40 @@
 import { ApolloServer } from '@apollo/server'
-import { startStandaloneServer } from '@apollo/server/standalone'
+import { expressMiddleware } from '@apollo/server/express4'
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
+import express from 'express'
+import http from 'http'
+import cors from 'cors'
+import pkg from 'body-parser'
+import { resolvers } from './resolvers'
+import typeDefs from './typeDefs'
 
-const books = [
-  {
-    title: 'The Awakening',
-    author: 'Kate Chopin',
-  },
-  {
-    title: 'City of Glass',
-    author: 'Paul Auster',
-  },
-]
+const { json } = pkg
 
-const typeDefs = `
-  type Book {
-    title: String
-    author: String
-  }
-
-  type Query {
-    books: [Book]
-  }
-`
-
-const resolvers = {
-  Query: {
-    books: () => books,
-  },
+interface MyContext {
+  token?: String
 }
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-})
+void (async () => {
+  const app = express()
+  const httpServer = http.createServer(app)
 
-const { url } = await startStandaloneServer(server)
+  const server = new ApolloServer<MyContext>({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  })
 
-console.log(`🚀 Server ready at ${url}`)
+  await server.start()
+
+  app.use(
+    '/graphql',
+    cors<cors.CorsRequest>(),
+    json(),
+    expressMiddleware(server, {
+      context: async ({ req }) => ({ token: req.headers.token }),
+    })
+  )
+
+  await new Promise<void>((resolve) => httpServer.listen({ port: 4000 }, resolve))
+  console.log(`🚀 Server ready at http://localhost:4000/graphql`)
+})()
