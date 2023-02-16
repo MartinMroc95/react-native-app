094354696ffcde34f336f79e1e5669c2d66fe411
Martin Mroc <mato.mroc@gmail.com>
2023-02-16 15:03:52 +0100

    Graphql server part 1

---
+++ api/src/graphql/Link.ts
@@ -0,0 +1,65 @@
+import { extendType, nonNull, objectType, stringArg } from 'nexus'
+import { NexusGenObjects } from '../../nexus-typegen'
+
+export const Link = objectType({
+  name: 'Link',
+  definition(t) {
+    t.nonNull.int('id')
+    t.nonNull.string('description')
+    t.nonNull.string('url')
+  },
+})
+
+const links: NexusGenObjects['Link'][] = [
+  {
+    id: 1,
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL',
+  },
+  {
+    id: 2,
+    url: 'graphql.org',
+    description: 'GraphQL official website',
+  },
+]
+
+export const LinkQuery = extendType({
+  type: 'Query',
+  definition(t) {
+    t.nonNull.list.nonNull.field('feed', {
+      type: 'Link',
+      resolve(parent, args, context, info) {
+        return links
+      },
+    })
+  },
+})
+
+export const LinkMutation = extendType({
+  // 1
+  type: 'Mutation',
+  definition(t) {
+    t.nonNull.field('post', {
+      // 2
+      type: 'Link',
+      args: {
+        // 3
+        description: nonNull(stringArg()),
+        url: nonNull(stringArg()),
+      },
+
+      resolve(parent, args, context) {
+        const { description, url } = args // 4
+
+        let idCount = links.length + 1 // 5
+        const link = {
+          id: idCount,
+          description: description,
+          url: url,
+        }
+        links.push(link)
+        return link
+      },
+    })
+  },
+})
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,24 +1,11 @@
 import { ApolloServer } from 'apollo-server'
 
-const typeDefs = `
-  type Query {
-    info: String!
-    hello(name: String): String
-  }
-`
-
-const resolvers = {
-  Query: {
-    hello: (_: any, args: { name: string }) => `Hello ${args.name || 'World!'}`,
-  },
-}
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+import { schema } from './schema'
+export const server = new ApolloServer({
+  schema,
 })
 
-const port = 5000
+const port = 4000
 
 server.listen({ port }).then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`)
